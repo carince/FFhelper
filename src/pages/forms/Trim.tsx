f345c2f5ddd8eb7643f3881fb99e85a88c0d1679
Norikiru <norikiru.official@gmail.com>
2023-03-04 04:19:07 +0800

    trim: add disabling of form when toggling

--- src/pages/forms/Trim.tsx
+++ src/pages/forms/Trim.tsx
@@ -1,35 +1,44 @@
+import { useState } from 'react'
 import { Container, Form, Row, Col } from 'react-bootstrap'
 
 export default function Trim() {
+    const [isDisabled, disableForm] = useState(true)
+
     return (
         <Container className="Trim border rounded shadow-sm p-3">
             <Row className="Trim-Title">
                 <Col className="d-flex justify-content-between align-items-center">
                     <h2> Trim </h2>
                     <Form.Check type="switch"
-                        id="custom-switch"
                         className="float-right"
                         onChange={(elm) => {
-                            window.settings.update("trimConfirm", elm.target.checked);
+                            window.settings.update("trimConfirm", elm.target.checked)
+                            if (elm.target.checked === true) {
+                                disableForm(false)
+                            } else {
+                                disableForm(true)
+                            }
                         }}
                     />
                 </Col>
                 <p className='fs-6'> Trim the video to your specified time. </p>
             </Row>
             <Form className="Trim-Body">
-                <Form.Group>
-                    <Form.Label> Start Timestamp </Form.Label>
-                    <Form.Control type="input" placeholder="HH:MM:SS" onChange={(elm) => {
-                        window.settings.update("trimStart", elm.target.value);
-                    }} />
-                </Form.Group>
+                <fieldset disabled={isDisabled}>
+                    <Form.Group>
+                        <Form.Label> Start Timestamp </Form.Label>
+                        <Form.Control type="input" placeholder="HH:MM:SS" onChange={(elm) => {
+                            window.settings.update("trimStart", elm.target.value);
+                        }} />
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label> End Timestamp </Form.Label>
-                    <Form.Control type="input" placeholder="HH:MM:SS" onChange={(elm) => {
-                        window.settings.update("trimEnd", elm.target.value);
-                    }} />
-                </Form.Group>
+                    <Form.Group>
+                        <Form.Label> End Timestamp </Form.Label>
+                        <Form.Control type="input" placeholder="HH:MM:SS" onChange={(elm) => {
+                            window.settings.update("trimEnd", elm.target.value);
+                        }} />
+                    </Form.Group>
+                </fieldset>
             </Form>
         </Container>
     )
